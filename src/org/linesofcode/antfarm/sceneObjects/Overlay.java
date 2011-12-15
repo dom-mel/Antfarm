9399737be65826e79a283396ccb8380716630881
Dominik Eckelmann <deckelmann@gmail.com>
2011-12-15 20:57:11 +0100

    fixed overlay key, closes #7

--- src/org/linesofcode/antfarm/sceneObjects/Overlay.java
+++ src/org/linesofcode/antfarm/sceneObjects/Overlay.java
@@ -5,33 +5,36 @@ import controlP5.Slider;
 import org.linesofcode.antfarm.AntFarm;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * @author Dominik Eckelmann
  */
 public class Overlay implements SceneObject {
 
-    private ControlP5 controlP5;
-    private AntFarm antFarm;
+    private static final float OVERLAY_KEY_BLOCK = 0.2f;
+    private static final char OVERLAY_KEY = 'o';
 
-    private Map<String, Slider> sliders;
+    private final ControlP5 controlP5;
+    private final AntFarm antFarm;
+
+    private final Map<String, Slider> sliders;
     private int sliderCount;
+    private float block;
 
     private boolean visible;
 
-    public Overlay(AntFarm antFarm) {
+    public Overlay(final AntFarm antFarm) {
         this.antFarm = antFarm;
         controlP5 = new ControlP5(antFarm);
         sliders = new HashMap<String, Slider>();
         sliderCount = 0;
         visible = false;
+        block = 0;
     }
 
-    public Slider addSlider(String name, float min, float max, float defaultValue) {
-        Slider slider = controlP5.addSlider(name, min, max, defaultValue, 10, sliderCount, 100, 10);
+    public Slider addSlider(final String name, final float min, final float max, final float defaultValue) {
+        final Slider slider = controlP5.addSlider(name, min, max, defaultValue, 10, sliderCount, 100, 10);
         if (!visible) {
             slider.hide();
         }
@@ -42,7 +45,7 @@ public class Overlay implements SceneObject {
 
     @Override
     public void draw() {
-        if (visible == false) {
+        if (!visible) {
             return;
         }
 
@@ -53,9 +56,10 @@ public class Overlay implements SceneObject {
     }
 
     @Override
-    public void update(float delta) {
-        if (antFarm.keyPressed) {
-            if (antFarm.key == 's') {
+    public void update(final float delta) {
+        block += delta;
+        if (block > OVERLAY_KEY_BLOCK && antFarm.keyPressed) {
+            if (antFarm.key == OVERLAY_KEY) {
                 visible = !visible;
                 if (visible) {
                     showSlider();
@@ -63,17 +67,18 @@ public class Overlay implements SceneObject {
                     hideSlider();
                 }
             }
+            block = 0;
         }
     }
 
     private void showSlider() {
-        for (Slider slider : sliders.values()) {
+        for (final Slider slider : sliders.values()) {
             slider.show();
         }
     }
 
     private void hideSlider() {
-        for (Slider slider : sliders.values()) {
+        for (final Slider slider : sliders.values()) {
             slider.hide();
         }
     }
